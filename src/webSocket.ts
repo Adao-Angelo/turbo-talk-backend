b1ff84936a6d1326b8e31de7f0cf6d1acc681a8a
Adao-Angelo <adaobegginer@gmail.com>
2024-01-25 12:12:02 +0100

    resolved the bug multer user a same   roon

--- src/webSocket.ts
+++ src/webSocket.ts
@@ -1,10 +1,29 @@
 import { io } from "./http";
 
+interface RoonUser {
+  socket_id: string;
+  username: string;
+  roon: string;
+}
+
+const users: RoonUser[] = [];
+
 io.on("connection", (socket) => {
-  socket.on("select_roon", (datas) => {
-    console.log(datas);
-  });
-  socket.on("message", (datas_msg) => {
-    console.log(datas_msg);
+  socket.on("select_roon", (data) => {
+    socket.join(data.roon);
+    const userRoon = users.find(
+      (user) => user.username === data.username && user.roon === data.roon
+    );
+    if (userRoon) {
+      userRoon.socket_id = socket.id;
+    } else {
+      users.push({
+        socket_id: socket.id,
+        username: data.username,
+        roon: data.roon,
+      });
+    }
+
+    console.log(users);
   });
 });
