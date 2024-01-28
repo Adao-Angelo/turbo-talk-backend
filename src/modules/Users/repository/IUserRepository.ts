e77669aee1124d34859644d8ca8a28155769f928
Adao-Angelo <adaobegginer@gmail.com>
2024-01-28 19:03:26 +0100

    create a Interface User Repositoryy

---
+++ src/modules/Users/repository/IUserRepository.ts
@@ -0,0 +1,10 @@
+interface ICreateUserSessionDTO {
+  socket_id: string;
+  username: string;
+  roon: string;
+}
+
+interface IUserRepository {
+  CreteSession(userRoon: ICreateUserSessionDTO): void;
+}
+export { IUserRepository, ICreateUserSessionDTO };
