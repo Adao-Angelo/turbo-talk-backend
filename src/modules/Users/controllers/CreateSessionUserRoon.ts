ee6cf7b7c2d01caec7e26cc74dd904d9c8858584
Adao-Angelo <adaobegginer@gmail.com>
2024-01-28 23:58:19 +0100

    create a controller user

--- src/modules/Users/controllers/CreateSessionUserRoon.ts
+++ src/modules/Users/controllers/CreateSessionUserRoon.ts
@@ -1,10 +1,8 @@
+import { IUserRepository } from "../repository/IUserRepository";
 import { userRepository } from "../repository/UersRepository";
 
+class CreateUserSessionController {
+  constructor(private userR: IUserRepository) {}
 
-class CreateUserSessionController{
-
-    constructor(){
-        
-    }
-
+  execute() {}
 }
