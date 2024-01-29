f1f9c321665911fb68266887f2d82a661e013d30
Adao-Angelo <adaobegginer@gmail.com>
2024-01-29 23:27:01 +0100

    config the sign in

--- public/javascript/Home.js
+++ public/javascript/Home.js
@@ -2,10 +2,10 @@ export const socket = io();
 
 const msg = document.getElementById("message");
 const send_btn = document.querySelector(".send-message");
-
+const containerMessage = document.querySelector(".messages");
 const urlSearch = new URLSearchParams(window.location.search);
 const username = urlSearch.get("user");
-const roon = urlSearch.get("select_roon");
+const roon = urlSearch.get("roon");
 
 socket.emit(
   "select_roon",
@@ -13,8 +13,10 @@ socket.emit(
     username,
     roon,
   },
-  (response) => {
-    console.log(response);
+  (messages) => {
+    for (let message of messages) {
+      rederMessage(message);
+    }
   }
 );
 
@@ -25,6 +27,18 @@ send_btn.addEventListener("click", () => {
     msg: msg.value,
   };
   socket.emit("message", message);
-
   msg.value = "";
 });
+
+socket.on("message", (message) => {
+  rederMessage(message);
+});
+
+function rederMessage(message) {
+  containerMessage.innerHTML += `
+  <div class="message">
+    <h3>${message.user}</h3>
+    <p>${message.text}</p>
+    <span>${message.date}</span>
+  </div>`;
+}
